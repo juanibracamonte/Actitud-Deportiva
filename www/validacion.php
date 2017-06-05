4a18d2a182a3f486895a3c8665629375150e3083
Juani <juani@brillantideaz.com>
2017-06-05 15:26:18 -0300

    Login OK sin db

--- www/validacion.php
+++ www/validacion.php
@@ -4,8 +4,8 @@ en una verdadera aplicacion generalmente son dinamicos a partir de una base de d
 $usuarioValido = "A";
 $passwordValido = "a";
 /* Extrae los valores enviados desde la aplicacion movil */
-$usuarioEnviado = $_GET['nombredeusuario'];
-$passwordEnviado = $_GET['clave'];
+$usuarioEnviado = $_GET['usuario'];
+$passwordEnviado = $_GET['password'];
 /* crea un array con datos arbitrarios que seran enviados de vuelta a la aplicacion */
 
 /* verifica que el usuario y password concuerden correctamente */
@@ -13,6 +13,7 @@ if(  $usuarioEnviado == $usuarioValido  && $passwordEnviado == $passwordValido )
 	/*esta informacion se envia solo si la validacion es correcta */
 
 	$resultados["validacion"] = "ok";
+	
 }else{
 	/*esta informacion se envia si la validacion falla */
 	$resultados["mensaje"] = "Usuario y password incorrectos";
